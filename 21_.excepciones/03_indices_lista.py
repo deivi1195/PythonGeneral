ae5c561c9f953f28f539c2a21daebdde766fee77
deivi1195 <deivig1195@gmail.com>
2023-03-06 14:43:01 -0400

    Intento de acceso a indices negativos

--- 21_.excepciones/03_indices_lista.py
+++ 21_.excepciones/03_indices_lista.py
@@ -8,17 +8,29 @@ print(f'Primer elemento de la lista `lenguajes`: {lenguajes[0]}\n')
 indice = 6
 
 try:
-    print(f'Primer elemento de la lista `lenguajes`: {lenguajes[indice]}')
+    print(f'Ultimo elemento de la lista `lenguajes`: {lenguajes[indice]}')
 except IndexError:
     print(f'El indice {indice} no existe en la lista `lenguajes`')
     
 print('El programa ha finalizado.')
 
 
+# Intento de acceso a indices negativos:
 
+print('Intento de acceso a indices negativos')
 
+print(f'Ultimo elemento de la lista lenguajes: {lenguajes[-1]}')
 
+indice = -7
 
+try:
+    lenguaje = lenguajes[indice]
+    
+    print(f'\nPrimer elemento de la lista lenguajes: {lenguaje}')
+except:
+    print(f'\nEl indice {indice} no existe en la lista lenguajes.')
+    
+print("El Programa Python ha terminado.")
 
 
 
