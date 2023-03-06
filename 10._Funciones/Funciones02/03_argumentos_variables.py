0484ff1323fa8e1f753f44f661039886a281a9a9
deivi1195 <deivig1195@gmail.com>
2023-03-06 17:17:21 -0400

    Lista de variable de argumentos de una funcion
    (*valores)
    funcion con n cantidad de argumentos

---
+++ 10._Funciones/Funciones02/03_argumentos_variables.py
@@ -0,0 +1,36 @@
+# Lista de variable de argumentos de una funcion
+print('Lista de variable de argumentos de una funcion')
+
+# def sumar(a, b):
+#     return a + b
+
+# def sumar(a, b, c):
+#     return a + b + c
+
+# def sumar(a, b, c, d):
+#     return a + b + c + d
+
+# def sumar(a, b, c, d, e):
+#     return a + b + c + d + e
+
+#Todas estas funciones se pueden hacer en una sola
+
+# *valores = pueden ser 0 o mas argumentos (n cantidad de argumentos)
+# se pueden iterar
+def sumar(*valores):
+    suma = 0
+    
+    for v in valores:
+        suma += v
+        
+    return suma
+
+resultado = sumar(1)
+print(f'El resultado de la suma es igual: {resultado}')
+
+resultado = sumar(1, 2)
+print(f'El resultado de la suma es igual: {resultado}')
+
+resultado = sumar(1, 2, 3)
+print(f'El resultado de la suma es igual: {resultado}')
+
