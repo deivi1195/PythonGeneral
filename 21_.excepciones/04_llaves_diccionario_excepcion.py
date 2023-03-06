1dda341c4f769964ca212609cd590578d96a20a2
deivi1195 <deivig1195@gmail.com>
2023-03-06 14:52:41 -0400

    Excepciones a la hora de trabajar con diccionarios

---
+++ 21_.excepciones/04_llaves_diccionario_excepcion.py
@@ -0,0 +1,24 @@
+# Excepciones a la hora de trabajar con diccionarios
+
+versiones = {
+    'Python': '3.8.1',
+    'Java': '12',
+    'JavaScript': 'ES6',
+    'C#': '8'
+}
+
+lenguajes = input('Escriba un nombre de lenguaje de programacion: ')
+
+try:
+    version = versiones[lenguajes]
+
+    print(f'La version de {lenguajes} es {version}')
+except KeyError as e:
+    print(f'ERROR: {e}')
+
+print("Fin del Programa.")
+
+
+
+
+
