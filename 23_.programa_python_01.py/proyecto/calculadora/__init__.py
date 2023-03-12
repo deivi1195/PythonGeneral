e4e389639542be7aad19cdd0c8d1a06b1a4c058a
deivi1195 <deivig1195@gmail.com>
2023-03-12 18:37:18 -0400

    Comentario de la funcion de __init__.py

--- 23_.programa_python_01.py/proyecto/calculadora/__init__.py
+++ 23_.programa_python_01.py/proyecto/calculadora/__init__.py
@@ -1,3 +1,6 @@
+# Este archivo se utiliza para inicializar otros modulos
+# Cada vez que se cree un modulo se define este archivo, por convencion
+# De forma automatica se realiza su invocacion cada vez que se ejecute el proyecto.
 
 
 
