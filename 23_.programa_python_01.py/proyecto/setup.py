b7017cc6aa4fcf940ca081fc6187629c6f4eb6ed
deivi1195 <deivig1195@gmail.com>
2023-03-12 22:27:06 -0400

    Configuracion del archivo setup

--- 23_.programa_python_01.py/proyecto/setup.py
+++ 23_.programa_python_01.py/proyecto/setup.py
@@ -1,4 +1,12 @@
+from setuptools import setup
 
+setup(name='calculadora', version='1.0.0', packages=['calculadora'], 
+entry_points= {
+    #si tuvieramos una aplicacion con interfaz grafica
+    #se puede escribir 'gui_scripts
+    #['nombre de proyecto = donde esta alojado el proyecto.__main__:metodo del modulo(def main())]
+    'console_scripts': ['calculadora = calculadora.__main__:main']
+})
 
 
 
